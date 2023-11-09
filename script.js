98d5c8bd6c960ed5e3b532db01b608afcccf56f4
Paridhi <paridhigarg27@gmail.com>
2023-11-09 03:03:57 -0500

    Finalising layouts

--- script.js
+++ script.js
@@ -1,50 +1,23 @@
-// text.onmousedown = function(event) { //  start the process
-//     // get ready to move: make an absolute and top z-index
-//     text.style.position = 'absolute';
-//     text.style.zIndex = 1000;
-//     // move it from any existing parents directly to the body
-//     // to position it relative to the body
-//     document.body.append(text);
-//     // and put this absolutely positioned text under the pointer
-//     moveAt(event.pageX, event.pageY);
-
-//     // centers the text on the coordinates (pageX, pageY)
-//     function moveAt(pageX, pageY) {
-//       text.style.left = pageX - text.offsetWidth / 2 + 'px';
-//       text.style.top = pageY - text.offsetHeight / 2 + 'px';
-//     }
-//     function onMouseMove(event) {
-//       moveAt(event.pageX, event.pageY);
-//     }
-//     //  move the text on mousemove
-//     document.addEventListener('mousemove', onMouseMove);
-//     // drop the text, remove unneeded handlers
-//     document.onmouseup = function() {
-//       document.removeEventListener('mousemove', onMouseMove);
-//       text.onmouseup = null;
-//     };
-//   };
-
-  var x = document.createElement("BUTTON");
-
-  function play() {
+function play() {
     var audio = document.getElementById("audio");
     audio.play();
-  }
+}
 
-  function displaySmells() {
+function displaySmells() {
     document.querySelector(".textField").innerHTML =
     "hi";
     text.style.display = "block";
-  }
+}
 
-  function displayImg(){
+function displayImg(){
     
-  }
+}
 
 // draggable image journal:
 dragElement(document.getElementById("pic"));
 dragElement(document.getElementById("pic2"));
+dragElement(document.getElementById("pic3"));
+dragElement(document.getElementById("pic5"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -86,3 +59,9 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+let image_node = document.createElement('i1');
+      i1.addEventListener('click', () => {
+        i1.parentNode.removeChild(i1);
+
+      });
